855e7cac79721a7806f1dd2eb04eb5c556484b09
kamiazya <yuki@kamiazya.tech>
2020-10-12 23:24:00 +0900

    fix build setting

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,17 @@
 import typescript from 'rollup-plugin-typescript2';
-import dts from 'rollup-plugin-dts';
 
-export default [
-  {
-    input: './src/index.ts',
-    output: [
-      {
-        format: 'cjs',
-        file: './lib/index.js',
-      },
-      {
-        format: 'esm',
-        file: './lib/index.mjs',
-      },
-    ],
-    plugins: [typescript()],
-    external: ['ts-graphviz', 'child_process', 'tmp-promise', 'util', 'fs'],
-  },
-  {
-    input: './lib/index.d.ts',
-    output: [{ file: 'lib/index.d.ts', format: 'es' }],
-    plugins: [dts()],
-  },
-];
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      format: 'cjs',
+      file: './lib/index.js',
+    },
+    {
+      format: 'esm',
+      file: './lib/index.mjs',
+    },
+  ],
+  plugins: [typescript()],
+  external: ['ts-graphviz', 'child_process', 'tmp-promise', 'util', 'fs'],
+};
